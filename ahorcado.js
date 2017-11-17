29294fd8a2705139671f57918e11cb1d559122ef
Eric <erflag96@gmail.com>
2017-11-17 12:38:23 +0100

    Cambio de Incial

--- ahorcado.js
+++ ahorcado.js
@@ -1,12 +1,1 @@
-var letra1 ="a";
-var letra2 ="";
-
-letra2 = prompt("Introduce una letra" , "");
-
-if (letra1 == letra2){
-	alert ("Has acertado!!!");
-}
-
-else {
-	alert ("Error");
-}
+JS JS JS JS JSJ SJ SJS 
